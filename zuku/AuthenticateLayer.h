aff1f9b07b9b0cf622ba004e6cff02715e78a546
Ron Schachtner <wargfn@gmail.com>
2012-03-04 10:53:40 -0500

    Added Authentication layers to the game

---
+++ zuku/AuthenticateLayer.h
@@ -0,0 +1,16 @@
+//
+//  AuthenticateLayer.h
+//  zuku
+//
+//  Created by Ron Schachtner on 3/4/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface AuthenticateLayer : CCLayer {
+    
+}
+
+@end
---
+++ zuku/AuthenticateLayer.m
@@ -0,0 +1,14 @@
+//
+//  AuthenticateLayer.m
+//  zuku
+//
+//  Created by Ron Schachtner on 3/4/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AuthenticateLayer.h"
+
+
+@implementation AuthenticateLayer
+
+@end
