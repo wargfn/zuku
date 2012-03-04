c8effe4b9810ed8d3571f52e70b8413b6813886f
Ron Schachtner <wargfn@gmail.com>
2012-03-04 10:59:57 -0500

    Added Status.h - notes to myself on where I am at.

---
+++ zuku/Status_Notes.h
@@ -0,0 +1,30 @@
+//
+//  Header.h
+//  zuku
+//
+//  Created by Ron Schachtner on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+// File to Keep track of Tasks and Notes
+//
+// -- - Initial Project Setup & Initial icons
+// -- - Based on a Cocos2D project for movie manipulation
+// -- - Still pointed at HelloWorldLayer
+// 00 - Initial Copies of GCTurnBasedMatchHelper
+// 01 - Implementation of GCTurnBasedMatchHelper
+// 02 - Implementation of GK Authentication, seeing "Welcome Back Player" on Hello World Screen
+// 03 - Retun Varibles for GKPlayerID into Cocos2d text Label on Hello World Layer
+// 04 - WelcomeScreen added to reat players after they are authenticated.  Going to show an error screen when players cancel out of Game Center, everything requires it.
+// Flow is HelloWorld, Authenticate, then Welcome Screen to go to Game Center to play, Weclome Screen = Main menu
+// 05 - Welcome Screen move people to Game Center - Testing
+
+// Trashed all changes. 3/4/2012
+// Changed implementations into version 2.x which has direct GameKit integrations
+// -- inital Project Setup & icons
+// -- setup game sceens to push me through what was going on with GameKit
+// -- Created Basic display pages to roll through the GameKit.
+// -- Changes to Authentication and to force player authentication
+
+#ifndef zuku_Header_h
+#define zuku_Header_h
+#endif
