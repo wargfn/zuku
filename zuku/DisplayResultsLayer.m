8749d327a8e4f16e712888659096ce87a7fbcc56
Ron Schachtner <wargfn@gmail.com>
2012-04-11 13:32:37 -0500

    Beginning of Layers for Results

--- zuku/DisplayResultsLayer.m
+++ zuku/DisplayResultsLayer.m
@@ -142,10 +142,14 @@
         
         [self displayMatchData];
         
+        /*
+        // Want to Add Multiple Layers KEEP REading
+         
         CCSprite *frame = [CCSprite spriteWithFile:@"frame.png"];
         frame.position = ccp( size.width /2, size.height /2);
         
         [self addChild:frame z:40 tag:1101];
+         */
         
         //need display functions HERE
         
